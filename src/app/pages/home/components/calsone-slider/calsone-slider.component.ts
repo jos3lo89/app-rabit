d2a5269eb363fd3f5227bff4122ed11512ac0f15
jos3lo89 <gothangel21@hotmail.com>
2024-11-27 02:43:25 -0500

    newCommit

--- src/app/pages/home/components/calsone-slider/calsone-slider.component.ts
+++ src/app/pages/home/components/calsone-slider/calsone-slider.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from "@ionic/angular"
 
 @Component({
   selector: 'app-calsone-slider',
   templateUrl: './calsone-slider.component.html',
   styleUrls: ['./calsone-slider.component.scss'],
+  standalone: true,
+  imports: [IonicModule]
 })
 export class CalsoneSliderComponent {
 
